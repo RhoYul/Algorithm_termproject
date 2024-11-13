251f4a6efc54cc983aeadaff94a056cfbadbc8c8
Moon Sunyu <130819577+moonsunyu@users.noreply.github.com>
2024-11-14 02:21:56 +0900

    Update settings_tab.dart

--- Front/screen/settings_tab.dart
+++ Front/screen/settings_tab.dart
@@ -1,27 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:term_project/settings_subject/subject_management.dart';
+import 'package:term_project/settings_category/SubjectListScreen.dart';
+import 'package:term_project/settings_category/GeneralScheduleScreen.dart';
 
 class SettingsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Settings"),
+        title: Text('설정'),
       ),
       body: ListView(
         children: [
           ListTile(
-            leading: Icon(Icons.book),
-            title: Text("과목 관리"),
+            title: Text('카테고리 관리'),
             trailing: Icon(Icons.arrow_forward),
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => SubjectManagementScreen()),
+                MaterialPageRoute(builder: (context) => CategoryManagementScreen()),
               );
             },
           ),
-          // Other settings options can go here
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryManagementScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('카테고리 관리'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('과목 관리'),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SubjectListScreen()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('일반 일정 관리'),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GeneralScheduleScreen()),
+              );
+            },
+          ),
         ],
       ),
     );
